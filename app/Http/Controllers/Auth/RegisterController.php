bb45b5362cb2995eb7093d2c020121342de4824b
Freddy Perez <freddy.perez.trabajos@gmail.com>
2021-04-13 17:53:24 -0400

    reparando el register 1.4

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        dd($data);
+        //dd($data);
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
